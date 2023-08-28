10c74f70d63895019646e62c8380d16a1b9ebe35
Adarsh Jha <99401146+adarsh-jha1611@users.noreply.github.com>
2023-08-28 23:41:23 +0530

    Update Banner.js

--- Frontend/src/components/Banner/Banner.js
+++ Frontend/src/components/Banner/Banner.js
@@ -4,7 +4,7 @@ import Carousel from "./Carousel";
 const useStyles = makeStyles((theme) => ({
   banner: {
     backgroundImage: "url(./banner2.jpg)",
-  },
+  }, 
   bannerContent: {
     height: 400,
     display: "flex",
